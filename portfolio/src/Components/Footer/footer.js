a5f7d00947b7bb52896bee47fe4f5174ab548fb2
darrinim <darrinj.im@gmail.com>
2019-10-30 23:39:55 -0400

    finalized all sizes

--- portfolio/src/Components/Footer/footer.js
+++ portfolio/src/Components/Footer/footer.js
@@ -6,16 +6,17 @@ const Footer = () => {
   return (
     <div className="footerContainer">
       <h2 className="footerHeadline">Let's accomplish something great</h2>
-      <p className="footerSubtext">lorem ipsum about things that I can do and how we can collaborate and yadda yadda yaddaaaa</p>
+      <p className="footerSubtext">Would you like to work together? I would too! Shoot me a message detailing what you are looking for and I will be sure to get in touch with you.</p>
+      <p className="footerSubtext">Want to keep up to date with me and connect professionally? Give me a follow on LinkedIn below!</p>
       <div className="contactContainer">
         <img className="bitmojiFooter" src="https://res.cloudinary.com/darrin-im/image/upload/v1572408505/bitmoji-peek_bartez.png" />
-      <button className="contactButton"><a className="contactLink" href="mailto:darrinj.im@gmail.com">get in touch</a></button>
+        <a className="button contactLink" href="mailto:darrinj.im@gmail.com">get in touch</a>
       </div>
       <div className="contactFooterLinks">
-        <a target= "blank" href="https://www.linkedin.com/in/darrin-im/"><i class="fab fa-linkedin fa-1x"></i></a>
-        <a target= "blank" href="https://github.com/darrinim"><i class="fab fa-github-square fa-1x"></i></a>
-        <a><i class="fas fa-envelope fa-1x"></i></a>
-        <a><i class="fas fa-file fa-1x"></i></a>
+        <a className="contactIcon" target= "blank" href="https://www.linkedin.com/in/darrin-im/"><i class="fab fa-linkedin fa-1x"></i></a>
+        <a className="contactIcon" target= "blank" href="https://github.com/darrinim"><i class="fab fa-github-square fa-1x"></i></a>
+        <a className="contactIcon" target = "blank" href="mailto:darrinj.im@gmail.com"><i class="fas fa-envelope fa-1x"></i></a>
+        <a className="contactIcon" target = "blank" href="https://pdfhost.io/v/2X3sljwd4_ResumenewfinalPDF.pdf"><i class="fas fa-file fa-1x"></i></a>
       </div>
     </div>
   )
